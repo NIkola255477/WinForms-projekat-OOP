b95bf1e641e4fa92853bd1dec2429698aea26154
NIkola255477 <nedeljkovnikola81@gmail.com>
2025-12-03 13:57:24 +0100

    Add files via upload

---
+++ oop-p-k/json.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace oop_p_k
+{
+    public class json
+    {
+        public static string FileAdmin = "admin.txt";
+        public static string FileAvioni = "avioni.txt";
+        public static string FileLetovi = "letovi.txt";
+        public static string FileKorisnici = "korisnici.txt";
+        public static void SacuvajA(Dictionary<int, admin> Admini)
+        {
+            string json = JsonConvert.SerializeObject(Admini, Formatting.Indented);
+            File.WriteAllText(FileAdmin, json);
+        }
+        public static void SacuvajK(Dictionary<int, korisnik> korisnici)
+        {
+            string json = JsonConvert.SerializeObject(korisnici, Formatting.Indented);
+            File.WriteAllText(FileKorisnici, json);
+        }
+        public static void SacuvajL(Dictionary<int, Let> Letovi)
+        {
+            string json = JsonConvert.SerializeObject(Letovi, Formatting.Indented);
+            File.WriteAllText(FileLetovi, json);
+        }
+        public static void SacuvajAv(Dictionary<int, Avion> Avioni)
+        {
+            string json = JsonConvert.SerializeObject(Avioni, Formatting.Indented);
+            File.WriteAllText(FileAvioni, json);
+        }
+        public static void Sacuvaj(Dictionary<int, admin> Admini, Dictionary<int, korisnik> korisnici
+                       , Dictionary<int, Let> Letovi, Dictionary<int, Avion> Avioni)
+        {
+            SacuvajA(Admini);
+            SacuvajK(korisnici);
+            SacuvajL(Letovi);
+            SacuvajAv(Avioni);
+        }
+        public static void Ucitaj(Dictionary<int,admin> Admini, Dictionary<int, korisnik> korisnici
+            , Dictionary<int, Let> Letovi, Dictionary<int, Avion> Avioni)
+
+        {
+            if (!File.Exists(FileLetovi) || !File.Exists(FileAvioni) || !File.Exists(FileAdmin) || File.Exists(FileKorisnici))
+            {
+                return;
+            }
+            string jsonl = File.ReadAllText(FileLetovi);
+            string jsona = File.ReadAllText(FileAvioni);
+            string jsonad = File.ReadAllText(FileAdmin);
+            string jsonk = File.ReadAllText(FileKorisnici);
+            korisnici = JsonConvert.DeserializeObject<Dictionary<int, korisnik>>(jsonk);
+            Admini = JsonConvert.DeserializeObject<Dictionary<int, admin>>(jsonad);
+            Avioni = JsonConvert.DeserializeObject<Dictionary<int, Avion>>(jsona);
+            Letovi = JsonConvert.DeserializeObject<Dictionary<int, Let>>(jsonl);
+        }
+    }
+}
---
+++ oop-p-k/objekti.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oop_p_k
+{
+    public class admin
+    {
+        protected string Username { get; set; }
+        protected string Password { get; set; }
+        public admin(string Username, string Password)
+        {
+            this.Username = Username;
+            this.Password = Password;
+        }
+        public bool ProveraLozinke(string unosLozinke)
+        {
+            return unosLozinke == Password;
+        }
+        public bool ProveraUsera(string unosUsera)
+        {
+            return unosUsera == Username;
+        }
+        public void PromeniLozinku(string novaLozinka)
+        {
+            Password = novaLozinka;
+        }
+    }
+    public class korisnik
+    {
+        protected string Ime { get; set; }
+        protected string Prezime { get; set; }
+        protected string Email { get; set; }
+        protected string Lozinka { get; set; }
+        protected string BrojTelefona { get; set; }
+        protected string BrojPasosa { get; set; }
+        protected List<Let> Rezervacije { get; set; }
+        public korisnik(string ime, string prezime, string email, string lozinka, string brojTelefona, string brojPasosa)
+        {
+            Ime = ime;
+            Prezime = prezime;
+            Email = email;
+            Lozinka = lozinka;
+            BrojTelefona = brojTelefona;
+            BrojPasosa = brojPasosa;
+            Rezervacije = new List<Let>();
+        }
+        public bool ProveraLozinke(string unosLozinke)
+        {
+            return unosLozinke == Lozinka;
+        }
+        public void PromeniLozinku(string novaLozinka)
+        {
+            Lozinka = novaLozinka;
+        }
+    }
+    public class Avion
+    {
+
+    }
+    public abstract class Let
+    {
+        protected string Polaziste { get; set; }
+        protected string Odrediste { get; set; }
+        protected DateTime VremePolaska { get; set; }
+        protected DateTime VremeDolaska { get; set; }
+        protected string BrojLeta { get; set; }
+        protected double BrojMesta { get; set; }
+        protected string Kompanija { get; set; }
+        public Let(string polaziste, string odrediste, DateTime vremePolaska, DateTime vremeDolaska, string brojLeta, double brojMesta, string kompanija)
+        {
+            Polaziste = polaziste;
+            Odrediste = odrediste;
+            VremePolaska = vremePolaska;
+            VremeDolaska = vremeDolaska;
+            BrojLeta = brojLeta;
+            BrojMesta = brojMesta;
+            Kompanija = kompanija;
+        }
+        public abstract double IzracunajTrajanjeLeta();
+        public abstract double IzracunajCenuKarte();
+        public abstract void RezervisiKartu(string imePutnika);
+        public abstract void OtkaziRezervaciju(string imePutnika);
+        public abstract void IzmeniLet(string novoPolaziste, string novoOdrediste, DateTime novoVremePolaska, DateTime novoVremeDolaska);
+
+    }
+    public class objekti
+    {
+        public static Dictionary<int, admin> Admini = new Dictionary<int, admin>();
+        public static Dictionary<int, korisnik> Korisnici = new Dictionary<int, korisnik>();
+        public static Dictionary<int, Avion> Avioni = new Dictionary<int, Avion>();
+        public static Dictionary<int, Let> Letovi = new Dictionary<int, Let>();
+    }
+}
