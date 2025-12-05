6021bd10e85ec7a8c9aebc797b72a976b0ab5608
Nikola Nedeljkov <nnedeljkov@smart.edu.rs>
2025-12-05 17:03:44 +0100

    jos gluposti

--- oop-p-k/Admin.Designer.cs
+++ oop-p-k/Admin.Designer.cs
@@ -56,7 +56,7 @@
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.BackColor = System.Drawing.Color.Transparent;
             this.button1.Location = new System.Drawing.Point(12, 477);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(440, 81);
@@ -67,7 +67,7 @@
             // 
             // button2
             // 
-            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.BackColor = System.Drawing.Color.Transparent;
             this.button2.Location = new System.Drawing.Point(458, 477);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(449, 81);
@@ -78,7 +78,7 @@
             // 
             // button3
             // 
-            this.button3.BackColor = System.Drawing.Color.LightCoral;
+            this.button3.BackColor = System.Drawing.Color.Transparent;
             this.button3.Location = new System.Drawing.Point(913, 477);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(411, 81);
@@ -89,7 +89,7 @@
             // 
             // button4
             // 
-            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.BackColor = System.Drawing.Color.Transparent;
             this.button4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button4.Location = new System.Drawing.Point(913, 651);
             this.button4.Name = "button4";
@@ -100,44 +100,48 @@
             // 
             // button5
             // 
+            this.button5.BackColor = System.Drawing.Color.Transparent;
             this.button5.Location = new System.Drawing.Point(12, 650);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(440, 81);
             this.button5.TabIndex = 5;
             this.button5.Text = "izmeni let";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button5.UseVisualStyleBackColor = false;
             // 
             // button6
             // 
+            this.button6.BackColor = System.Drawing.Color.Transparent;
             this.button6.Location = new System.Drawing.Point(458, 564);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(449, 81);
             this.button6.TabIndex = 6;
             this.button6.Text = "obrisi let";
-            this.button6.UseVisualStyleBackColor = true;
+            this.button6.UseVisualStyleBackColor = false;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
+            this.button7.BackColor = System.Drawing.Color.Transparent;
             this.button7.Location = new System.Drawing.Point(913, 564);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(411, 81);
             this.button7.TabIndex = 7;
             this.button7.Text = "dodaj let";
-            this.button7.UseVisualStyleBackColor = true;
+            this.button7.UseVisualStyleBackColor = false;
             // 
             // button8
             // 
+            this.button8.BackColor = System.Drawing.Color.Transparent;
             this.button8.Location = new System.Drawing.Point(458, 651);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(449, 80);
             this.button8.TabIndex = 8;
             this.button8.Text = "dodaj avion";
-            this.button8.UseVisualStyleBackColor = true;
+            this.button8.UseVisualStyleBackColor = false;
             // 
             // button9
             // 
-            this.button9.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button9.BackColor = System.Drawing.Color.Transparent;
             this.button9.Location = new System.Drawing.Point(12, 563);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(440, 81);
@@ -150,6 +154,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::oop_p_k.Properties.Resources.Nenaslovljeni_dizajn__12_;
             this.ClientSize = new System.Drawing.Size(1336, 743);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
